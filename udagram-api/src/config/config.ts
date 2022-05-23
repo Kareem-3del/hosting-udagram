d9a49556b20601c76efff12a82155e1b3d1eaec3
Kareem-3del <kareem.adel.zayed@gmail.com>
2022-05-23 12:25:41 +0200

    Edit eb

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -7,14 +7,14 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  db_port: Number(process.env.DB_PORT),
-  port: Number(process.env.PORT),
-  host: process.env.POSTGRES_HOST,
+  username: `${process.env.POSTGRES_USERNAME}` || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "01001314657",
+  database: process.env.POSTGRES_DB || "postgres",
+  db_port: Number(process.env.DB_PORT) || 5432,
+  port: Number(process.env.PORT) || 3000,
+  host: process.env.POSTGRES_HOST || "database-1.cdhpj0a6tb8z.eu-west-3.rds.amazonaws.com",
   dialect: "postgres",
-  aws_region: process.env.AWS_REGION || "us-east-1",
+  aws_region: process.env.AWS_REGION || "us-west-3",
   aws_profile: process.env.AWS_PROFILE || "default",
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
